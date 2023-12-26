aff85d91beaec9f8420a011ed2b3a0f2ce71edac
Poon <zeepengineer@gmail.com>
2023-12-26 15:33:41 +0700

    lab11_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci (int x){
+        if (x > 1)
+    {
+        return fibonacci (x-1) + fibonacci(x-2);
+    } else {
+        return x;
+    }
+return x;
 }
